7d101ad53af39d3f4a3c2c788073b424a5bc22e8
almeigab <gabrielalmeida1403@hotmail.com>
2023-06-06 00:14:14 -0300

    refactor(dpk): refactored function `deterministicPartitionKey`

--- dpk.js
+++ dpk.js
@@ -1,28 +1,28 @@
 const crypto = require("crypto");
 
+function hashString(str) {
+  return crypto.createHash("sha3-512").update(str).digest("hex");
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event.partitionKey) {
+    candidate = typeof event.partitionKey === "string" ? event.partitionKey : JSON.stringify(event.partitionKey);
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    candidate = hashString(data);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return hashString(candidate);
   }
+
   return candidate;
 };
